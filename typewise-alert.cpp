0dbdc57ec2415bb23a5b489be90cebb80baba26b
salinisatheesh <salini.radhika@philips.com>
2023-09-29 11:48:30 +0530

    done refactoring

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -11,43 +11,29 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   return NORMAL;
 }
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
   int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
+  if (coolingType == PASSIVE_COOLING) {
+    upperLimit = 35;
+  } else if (coolingType == HI_ACTIVE_COOLING) {
+    upperLimit = 45;
+  } else if (coolingType == MED_ACTIVE_COOLING) {
+    upperLimit = 40;
   }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+  return inferBreach(temperatureInC, 0, upperLimit);
 }
 
-void checkAndAlert(
-    AlertTarget alertTarget, EquipmentCharacter characteristic, double temperatureInC) {
 
-  BreachType breachType = classifyTemperatureBreach(
-    characteristic.coolingType, temperatureInC
-  );
+void checkAndAlert(AlertTarget alertTarget, EquipmentCharacter characteristic, double temperatureInC) {
+  BreachType breachType = classifyTemperatureBreach(characteristic.coolingType, temperatureInC);
 
-  switch(alertTarget) {
-    case TO_CONTROLLER:
-      sendToController(breachType);
-      break;
-    case TO_EMAIL:
-      sendToEmail(breachType);
-      break;
+  if (alertTarget == TO_CONTROLLER) {
+    sendToController(breachType);
+  } else if (alertTarget == TO_EMAIL) {
+    sendToEmail(breachType);
   }
 }
+
 
 void sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
